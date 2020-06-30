c2d142be1c059d1a1fc2e1ce4d972679b86aeb51
John Quimbo <johnanthony.quimbo@gmail.com>
2020-06-29 22:39:30 -0400

    First commit, recursive sidebar core functionality working

---
+++ src/Sidebar.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+
+/*
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
+
+**FIX COLORS LATER**
+
+const color = orange[500];
+*/ 
+
+
+
+function SidebarItem({ label, items, depthStep = 10, depth = 0, ...rest }) {
+  return (
+    <>
+      <ListItem button dense {...rest}>
+        <ListItemText style={{ paddingLeft: depth * depthStep }}>
+          <span>{label}</span>
+        </ListItemText>
+      </ListItem>
+      {Array.isArray(items) ? (
+        <List disablePadding dense>
+          {items.map((subItem) => (
+            <SidebarItem
+              key={subItem.name}
+              depth={depth + 1}
+              depthStep={depthStep}
+              {...subItem}
+            />
+          ))}
+        </List>
+      ) : null}
+    </>
+  )
+}
+
+function Sidebar({ items, depthStep, depth }) {
+  return (
+    <div className="sidebar">
+      <List disablePadding dense>
+        {items.map((sidebarItem, index) => (
+          <SidebarItem
+            key={`${sidebarItem.name}${index}`}
+            depthStep={depthStep}
+            depth={depth}
+            {...sidebarItem}
+          />
+        ))}
+      </List>
+      <Fab size="small" color= "color"  aria-label="Add" className="addButton">
+          <AddIcon />
+      </Fab>
+    </div>
+  )
+}
+export default Sidebar
